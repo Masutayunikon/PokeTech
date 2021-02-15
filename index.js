5dc46d6631aac5fb405c95a164603c7cbc6b3504
Masutayunikon <71890754+Masutayunikon@users.noreply.github.com>
2021-02-15 14:08:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const Discord = require('discord.js');
 const Pokedex = require('pokedex-promise-v2');
 const fs = require('fs');
 const P = new Pokedex();
+const rm = require('discord.js-reaction-menu')
 const client = new Discord.Client();
+
+const cmd = new Map;
 
 function get_nbr_of_pokemon_catch(pokedex) {
     let nbr = 0;
@@ -32,18 +35,18 @@ function create_new_user(msg) {
         ]}
 
     users.users.push(new_user_json);
-    fs.writeFile('./user.json', JSON.stringify(users, null, 4), (err) => {
+    fs.writeFile(`./${msg.author.id}.json`, JSON.stringify(users, null, 2), (err) => {
         if (err) {
             console.log("Error to save json file");
         } else {
-            console.log("Save new users in json files.");
-            console.log(users.users[0]);
+            console.log("Save new users json files.");
         }
     });
 }
 
-function refresh_user_json() {
-    fs.writeFile('./user.json', JSON.stringify(users, null, 4), (err) => {
+function refresh_user_json(id) {
+    let this_user = require(`./${id}.json`);
+    fs.writeFile(`./${id}.json`, JSON.stringify(this_user, null, 2), (err) => {
         if (err) {
             return (-1)
         }
@@ -85,14 +88,6 @@ function get_types(types) {
     }
     return type;
 }
-
-function get_good_name(name) {
-    if (config.wrong_name.includes(name, 0)) {
-        name = config.good_name[config.wrong_name.indexOf(name)];
-    }
-    return name;
-}
-
 function setChanceGetLegendary() {
     let nbr = getRandomIntInclusive(1, 649);
     if (config.legendary_array.includes(nbr, 0)) {
@@ -105,79 +100,124 @@ function setChanceGetLegendary() {
     return nbr;
 }
 
-client.on('ready', () => {
-    console.log( `Logged in as ${client.user.username}!`);
-});
-
-client.on(`message`, msg => {
-    if (msg.content === '^catch') {
+function catching(msg) {
+    if (!fs.existsSync(`./${msg.author.id}.json`)) {
+        msg.channel.send(`<@${msg.author.id}> you are not registered.`).catch(err => console.error(err));
+        return;
+    } else {
         let pokemon_id = setChanceGetLegendary();
-        P.getPokemonByName(pokemon_id).then(function(response) {
-                name = get_good_name(response.forms[0].name);
-                let color = "BLUE";
-                if (config.legendary_array.includes(pokemon_id, 0))
-                    color = "GOLD";
-                msg.channel.send(`**<@${msg.author.id}> catch ${response.forms[0].name}**`,
-                    {embed: {
-                                color: color,
-                                thumbnail: {url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon_id}.gif`},
-                                description:`Pokedex ID: ${response.id}\n` +
-                                            `Type: ${get_types(response.types)}\n` +
-                                            `Height: ${((parseFloat(response.height) / 10 ) * 3.281).toFixed(2)} feet\n` +
-                                            `Weight: ${(parseFloat(response.weight) * 2.205).toFixed(2)} lbs\n`}}).catch(err => console.error(err));
-            })
-            .catch(function(error) {
-                console.log('There was an ERROR: ', error);
-            });
-        let pokedex = users.users[where_is_user_in_json(msg.author.id)].user[2].pokedex;
+        P.getPokemonByName(pokemon_id).then(function (response) {
+            name = response.forms[0].name;
+            let color = "BLUE";
+            if (config.legendary_array.includes(pokemon_id, 0))
+                color = "GOLD";
+            msg.channel.send(`**<@${msg.author.id}> catch ${response.forms[0].name}**`,
+                {
+                    embed: {
+                        color: color,
+                        thumbnail: {url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon_id}.gif`},
+                        description: `Pokedex ID: ${response.id}\n` +
+                            `Type: ${get_types(response.types)}\n` +
+                            `Height: ${((parseFloat(response.height) / 10) * 3.281).toFixed(2)} feet\n` +
+                            `Weight: ${(parseFloat(response.weight) * 2.205).toFixed(2)} lbs\n`
+                    }
+                }).catch(err => console.error(err));
+        }).catch(function (error) {
+            console.log('There was an ERROR: ', error);
+        });
+        let this_user = require(`./${msg.author.id}.json`)
+        let pokedex = this_user.users[0].user[2].pokedex;
         pokedex.push(pokemon_id);
-        users.users[where_is_user_in_json(msg.author.id)].user[3].pokedex_completion = get_nbr_of_pokemon_catch(pokedex);
-        pokedex.sort(function(a, b){return a - b});
-        if (refresh_user_json(where_is_user_in_json()) === -1) {
+        this_user.users[0].user[3].pokedex_completion = get_nbr_of_pokemon_catch(pokedex);
+        pokedex.sort(function (a, b) {
+            return a - b
+        });
+
+        if (refresh_user_json(msg.author.id) === -1) {
             console.log(`An error for save pokemon ${pokemon_id} for player ${msg.author.id} in json files.`);
         } else {
             console.log(`Saving a new pokemon ${pokemon_id} for player ${msg.author.id} in json files with success.`);
         }
-        console.log(users.users[where_is_user_in_json(msg.author.id)].user[3]);
     }
-/*-----------------------------------------------------------------------------------------------------------------------*/
-    if (msg.content === "^register") {
-        if (user_exist(msg.author.id)) {
-            msg.channel.send(`<@${msg.author.id}> you have already an account.`)
-        } else {
-            create_new_user(msg);
-            msg.channel.send(`<@${msg.author.id}> your account was created with success =D!`);
-        }
-    }
-/*-----------------------------------------------------------------------------------------------------------------------*/
-    if (msg.content === "^pokedex") {
-        let pokedex = users.users[where_is_user_in_json(msg.author.id)].user[2].pokedex;
+}
+
+function edit(msg) {
+    msg.edit("coucou");
+}
+
+
+function test(message) {
+
+}
+
+function see_pokedex(msg) {
+
+    if (!fs.existsSync(`./${msg.author.id}.json`)) {
+        msg.channel.send(`<@${msg.author.id}> you are not registered.`).catch(err => console.error(err));
+    } else {
+        let this_user = require(`./${msg.author.id}.json`);
+        let pokedex = this_user.users[0].user[2].pokedex;
         let all_name = "";
+        let i = 1;
+        let page = 2;
+        let arr = [];
         for (let i = 0; i < pokedex.length; i++) {
-            all_name += `${pokedex_name.pokedex_name[pokedex[i] - 1]} >> ${pokedex[i]}`
+            if (config.legendary_array.includes(pokedex[i], 0)) {
+                all_name += `**${pokedex_name.pokedex_name[pokedex[i] - 1]}** |<>| ***${pokedex[i]}***`
+            } else {
+                all_name += `${pokedex_name.pokedex_name[pokedex[i] - 1]} |<>| ***${pokedex[i]}***`
+            }
             all_name += '\n';
         }
-        const [first, ...rest] = Util.splitMessage(all_name, {maxLength: 512});
-        const embed = new MessageEmbed()
-            .setColor("BLUE")
-            .addField("page 1", first, true);
-        let i = 1;
-        let page = 2
-        ;
+
+        const [first, ...rest] = Util.splitMessage(all_name, {maxLength: 256});
+        arr.push(new MessageEmbed({
+            title: `page ${1} ${this_user.users[0].user[3].pokedex_completion}/649`,
+            description: `${first}`
+        }));
+
         for (const text of rest) {
-            embed.addField(`page ${page} cmplt:${users.users[where_is_user_in_json(msg.author.id)].user[3].pokedex_completion}/649`, text, true);
-            i++;
+            arr.push(new MessageEmbed({
+                title: `page ${page} ${this_user.users[0].user[3].pokedex_completion}/649`,
+                description: `${text}`
+            }));
             page++;
-            if (i === 3) {
-                msg.channel.send(embed).catch(err => console.error(err));
-                embed.fields = [];
-                i = 0;
-            }
         }
-        if (i !== 0) {
-            msg.channel.send(embed).catch(err => console.error(err));
-        }
+        let pokedex_embed = new rm.menu({
+            channel: msg.channel,
+            userID: msg.author.id,
+            pages: arr
+        })
     }
+}
+
+function register_user(msg) {
+    if (user_exist(msg.author.id)) {
+        msg.channel.send(`<@${msg.author.id}> you have already an account.`)
+    } else {
+        create_new_user(msg);
+        msg.channel.send(`<@${msg.author.id}> your account was created with success =D!`);
+    }
+}
+
+client.on('ready', () => {
+    console.log( `Logged in as ${client.user.username}!`);
+    cmd.set("catch", catching);
+    cmd.set("pokedex", see_pokedex);
+    cmd.set("register", register_user);
+    cmd.set("test", test);
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (cmd.get(command) == undefined)
+        message.channel.send(`<@${message.author.id}> wrong command!`).catch(err => console.error(err));
+    else
+        cmd.get(command)(message);
 });
 
 client.login(config.token).catch(err => console.error(err));
