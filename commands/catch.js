55506b64edc6c28bafe51a48963d4c82ced090e3
Yunikon <quentinmail974@gmail.com>
2022-06-06 00:49:59 +0400

    🔥catch command is fine, just need to make the probability of catching

--- commands/catch.js
+++ commands/catch.js
@@ -1,8 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { CommandInteraction } = require('discord.js');
+const { CommandInteraction, MessageEmbed, ColorResolvable, EmojiResolvable } = require('discord.js');
 const Pokedex = require('pokedex-promise-v2')
 const P = new Pokedex();
 const request = require('request');
+const fs = require('fs');
 
 async function getIdArray() {
     let result = [];
@@ -19,7 +20,7 @@ async function getIdArray() {
         }).catch(error => {
             console.error(error);
         })
-
+    console.log(result.length);
     return result;
 }
 
@@ -40,6 +41,169 @@ function getSprite(pokemon) {
     })
 }
 
+function findEmoji(name, guildId) {
+    const guild = client.guilds.cache.get(guildId);
+    if (!guild) return null;
+    return guild.emojis.cache.find(emoji => emoji.name === name);
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// function for read json file in user directory named with user id and return the json
+function readJsonFile(userId) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(`./users/${userId}.json`, (err, data) => {
+            if (err)
+                return reject(err);
+            return resolve(JSON.parse(data));
+        })
+    })
+}
+
+// function for create defaut json user with id. the default json contain an empty pokedex array, timer to catch set to timestamp now and level set to 1
+function createJsonFile(userId) {
+    return new Promise((resolve, reject) => {
+        const json = {
+            pokedex: [],
+            timer: Date.now(),
+            level: 1,
+            xp: 0
+        }
+        saveJsonFile(userId, json).then(() => {
+            resolve();
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+// function to return the difference between now and time of timer in json file
+function getTimer(userId) {
+    return new Promise((resolve, reject) => {
+        readJsonFile(userId).then(json => {
+            console.log(Date.now(), json.timer, Date.now() - json.timer, json.timer - Date.now());
+            return resolve(json.timer - Date.now());
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+// function to set 15 minute to time of now in json file
+function setTimer(userId) {
+    return new Promise((resolve, reject) => {
+        readJsonFile(userId).then(json => {
+            json.timer = Date.now() + 15 * 60 * 1000;
+            saveJsonFile(userId, json).then(() => {
+                resolve();
+            }).catch(error => {
+                reject(error);
+            })
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+// function to check if file exist with user id name in users folders
+function fileExist(userId) {
+    return new Promise((resolve, reject) => {
+        fs.access(`./users/${userId}.json`, fs.constants.F_OK, (err) => {
+            if (err)
+                return resolve(false);
+            return resolve(true);
+        })
+    })
+}
+
+// function for save json in user directory file named by user id
+function saveJsonFile(userId, json) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`./users/${userId}.json`, JSON.stringify(json), (err) => {
+            if (err)
+                return reject(err);
+            return resolve();
+        })
+    })
+}
+
+// function for read json file in user directory named with user id and add pokemon id to pokedex array
+function addPokemonToPokedex(userId, pokemonId) {
+    return new Promise((resolve, reject) => {
+        readJsonFile(userId).then(json => {
+            json.pokedex.push(pokemonId);
+            saveJsonFile(userId, json).then(() => {
+                resolve();
+            }).catch(error => {
+                reject(error);
+            })
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+// function get date given to minute and second string
+function getDate(date) {
+    return new Date(date).toLocaleString('fr-FR', {
+        minute: 'numeric',
+        second: 'numeric',
+        hour24: true
+    });
+}
+
+async function getRarity(id) {
+    return P.getPokemonSpeciesByName(id).then(response => {
+        if (response.is_mythical)
+            return "VIOLET";
+        else if (response.is_legendary)
+            return "YELLOW";
+        else if (response.is_baby)
+            return "GREEN";
+        return "BLUE";
+    }).catch(err => {
+       return "RED";
+    });
+}
+
+async function catchPokemon(interaction) {
+    if (await getTimer(interaction.user.id) < 0) {
+        //await setTimer(interaction.user.id);
+        let id = getRandomFromArray(await getIdArray());
+        P.getPokemonByName(id).then(async (pokemon) => {
+            addPokemonToPokedex(interaction.user.id, id).then(() => {
+                getSprite(pokemon).then(async (sprite) => {
+                    getRarity(id).then(async (rarity) => {
+                        const embed = new MessageEmbed()
+                            .setTitle(`${pokemon.name}`)
+                            // set description with pokemon information
+                            .setDescription(`**Type:** ${pokemon.types.map(type => type.type.name).join(', ')}\n**Weight:** ${pokemon.weight}kg\n**Height:** ${pokemon.height}m`)
+                            .setColor(rarity)
+                            .setImage(sprite)
+                        await interaction.editReply({embeds: [embed]});
+                    });
+                }).catch(error => {
+                    console.error(error);
+                    interaction.editReply(pokemon.sprites.front_default);
+                });
+            });
+        }).catch(error => {
+            console.error(error);
+        });
+    } else {
+        getTimer(interaction.user.id).then(async (time) => {
+            const embed = new MessageEmbed()
+                .setTitle(`You have to wait ${getDate(time)}`)
+                .setColor("BLUE")
+            await interaction.editReply({embeds: [embed]});
+        });
+    }
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('catch')
@@ -50,16 +214,16 @@ module.exports = {
      * @returns {Promise<void>}
      */
     async execute(interaction) {
-        let id = getRandomFromArray(await getIdArray());
-        interaction.reply("Catching...");
-        P.getPokemonByName(id).then(async (pokemon) => {
-            getSprite(pokemon).then(async (sprite) => {
-                interaction.editReply(sprite);
-            }).catch(error => {
-                interaction.editReply(pokemon.sprites.front_default);
-            });
+        await interaction.deferReply();
+        fileExist(interaction.user.id).then(async (result) => {
+            if (!result)
+                createJsonFile(interaction.user.id).then(async () => {
+                   await catchPokemon(interaction);
+                });
+            else
+                await catchPokemon(interaction);
         }).catch(error => {
-            console.error(error);
+           console.log(error);
         });
     },
 };
